ffd518aa576baa2873f565a439677302030efc2a
martatalmeida <martateixeiradealmeida@gmail.com>
2023-11-27 10:50:25 +0000

    Ex Pizzaria

--- FichaPratica12/src/Pizzaria/Main.java
+++ FichaPratica12/src/Pizzaria/Main.java
@@ -1,26 +1,34 @@
 package Pizzaria;
 
-import Pizzaria.Enums.BasePizza;
-import Pizzaria.Enums.OrigemIngrediente;
-import Pizzaria.Enums.TamanhoPizza;
-import Pizzaria.Enums.UnidadeMedida;
+import Pizzaria.Enums.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Base base = new Base("1","Massa Fina",UnidadeMedida.Gramas,5, BasePizza.Massa_Fina);
-        Topping ingrediente1 = new Topping("2", "Queijo", UnidadeMedida.Gramas,4, OrigemIngrediente.Nacional);
-        Topping ingrediente2 = new Topping("2", "Fiambre", UnidadeMedida.Gramas,4, OrigemIngrediente.Importado);
-        Topping ingrediente3 = new Topping("3", "Cogumelos",UnidadeMedida.Gramas,6,OrigemIngrediente.Nacional);
+        Queijo ingrediente1 = new Queijo("2", "Queijo", UnidadeMedida.Gramas,4, OrigemIngrediente.Nacional, TipoQueijo.Mozzarela);
+        Carne ingrediente2 = new Carne("2", "Fiambre", UnidadeMedida.Gramas,4, OrigemIngrediente.Importado,TipoCarne.Porco);
+        Vegetal ingrediente3 = new Vegetal("3", "Cogumelos",UnidadeMedida.Gramas,6,OrigemIngrediente.Nacional,TipoVegetal.Cogumelos);
+        Carne ingrediente4 = new Carne("4","Presunto",UnidadeMedida.Unidades, 15, OrigemIngrediente.Importado,TipoCarne.Porco);
+        FrutosdoMar ingrediente5 = new FrutosdoMar("5", "Camarão", UnidadeMedida.Unidades, 10, OrigemIngrediente.Nacional,TipoFrutosdoMar.Camarao);
+        Vegetal ingrediente6 = new Vegetal("6","Tomate",UnidadeMedida.Gramas,5,OrigemIngrediente.Nacional,TipoVegetal.Tomate);
+        Vegetal ingrediente7 = new Vegetal("7", "Pimento", UnidadeMedida.Gramas, 3, OrigemIngrediente.Importado, TipoVegetal.Pimento);
+
 
         IngredientePizza massa = new IngredientePizza(base, 200);
         IngredientePizza queijo = new IngredientePizza(ingrediente1,100);
         IngredientePizza fiambre = new IngredientePizza(ingrediente2,50);
         IngredientePizza cogumelos = new IngredientePizza(ingrediente3,30);
+        IngredientePizza presunto = new IngredientePizza(ingrediente4, 10);
+        IngredientePizza camarões = new IngredientePizza(ingrediente5,10);
+        IngredientePizza tomate = new IngredientePizza(ingrediente6, 100);
+        IngredientePizza pimento = new IngredientePizza(ingrediente7, 30);
 
 
         Pizza pizza1 = new Pizza(1, "Queijo e Fiambre","Queijo Mozzarella e Fiambre",8, TamanhoPizza.Média);
+        Pizza pizza2 = new Pizza(2, "Capriciosa", "Pizza variada",12,TamanhoPizza.Média);
+        Pizza pizza3 = new Pizza(3, "Vegetariana", "Pizza com cogumelos, tomate e pimento", 12, TamanhoPizza.Média);
 
         pizza1.adicionarIngredientes(massa);
         pizza1.adicionarIngredientes(queijo);
@@ -36,6 +44,29 @@ public class Main {
 
         System.out.println("");
         pizza1.detalhesPizza();
+        System.out.println("");
+        pizza1.definirTipoPizza();
+
+        pizza2.adicionarIngredientes(massa);
+        pizza2.adicionarIngredientes(tomate);
+        pizza2.adicionarIngredientes(queijo);
+        pizza2.adicionarIngredientes(presunto);
+        pizza2.adicionarIngredientes(camarões);
+
+        System.out.println("");
+        pizza2.detalhesPizza();
+        System.out.println("");
+        pizza2.definirTipoPizza();
+
+        pizza3.adicionarIngredientes(massa);
+        pizza3.adicionarIngredientes(tomate);
+        pizza3.adicionarIngredientes(cogumelos);
+        pizza3.adicionarIngredientes(pimento);
+
+        System.out.println("");
+        pizza3.detalhesPizza();
+        System.out.println("");
+        pizza3.definirTipoPizza();
 
     }
 
--- FichaPratica12/src/Pizzaria/Pizza.java
+++ FichaPratica12/src/Pizzaria/Pizza.java
@@ -34,13 +34,15 @@ public class Pizza {
         }
 
             if (ingrediente.getIngrediente() instanceof Topping) {
-                    if (this.ingredientes.size() > 0 && this.ingredientes.size() < 5) {
-                        this.ingredientes.add(ingrediente);
+                    if (this.ingredientes.size() > 0) {
+                        if (this.ingredientes.size() < 5) {
+                            this.ingredientes.add(ingrediente);
+                        } else {
+                            System.out.println("Não pode acrescentar mais toppings");
+                        }
                     } else {
-                        System.out.println("Não pode acrescentar mais toppings");
+                        System.out.println("Tem de acrescentar uma base primeiro.");
                     }
-                } else {
-                    System.out.println("Tem de acrescentar uma base primeiro.");
                 }
             }
 
@@ -71,8 +73,7 @@ public class Pizza {
         return totalCalorias;
     }
 
-    public void detalhesPizza()
-    {
+    public void detalhesPizza() {
         System.out.println("***** Pizza " + this.nome + " *****");
         System.out.println("Código: " + this.codigo);
         System.out.println("Descrição: " + this.descricao);
@@ -82,7 +83,67 @@ public class Pizza {
             System.out.print("Ingrediente " + (i+1) + ": ");
             ingredientes.get(i).detalhesIngredientesPizza();
         }
-
-
     }
+
+
+    public void definirTipoPizza(){
+        int contador = 0;
+        boolean carne = true;
+        for (int i = 1; i < ingredientes.size(); i++){
+            if (ingredientes.get(i).getIngrediente() instanceof Carne) {
+                carne = true;
+            } else {
+                carne = false;
+                break;
+            }
+        }
+        if (carne) {
+            System.out.println("Tipo: Pizza de Carne");
+        }
+        boolean mar = true;
+        for (int i = 1; i < ingredientes.size(); i++){
+            if (ingredientes.get(i).getIngrediente() instanceof FrutosdoMar) {
+                mar = true;
+            } else{
+                mar = false;
+                break;
+            }
+        }
+        if (mar) {
+            System.out.println("Tipo: Pizza do Mar");
+        }
+        boolean vegetariana = true;
+        for (int i = 1; i < ingredientes.size(); i++) {
+            if (ingredientes.get(i).getIngrediente() instanceof Vegetal){
+                vegetariana = true;
+            } else{
+                vegetariana = false;
+                break;
+            }
+        }
+        if (vegetariana){
+            System.out.println("Tipo: Pizza Vegetariana");
+        }
+        for (int i = 0; i < ingredientes.size(); i++){
+            if (ingredientes.get(i).getIngrediente() instanceof Carne){
+                contador++;
+            }
+            if (ingredientes.get(i).getIngrediente() instanceof FrutosdoMar){
+                contador++;
+            }
+            if (ingredientes.get(i).getIngrediente() instanceof Queijo){
+                contador++;
+            }
+            if (ingredientes.get(i).getIngrediente() instanceof Vegetal){
+                contador++;
+            }
+        }
+        if(contador >= 4){
+            System.out.println("Tipo: Pizza Completa");
+        }
+        if (!carne && !mar && !vegetariana && contador < 4){
+            System.out.println("Tipo: Pizza Mista");
+        }
+    }
+
 }
