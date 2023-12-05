ada64355543757b06c8063b6fe04cfc24d2d95b7
martatalmeida <martateixeiradealmeida@gmail.com>
2023-12-05 16:40:06 +0000

    WEB

--- WEB/ProjetoFinal/script.js
+++ WEB/ProjetoFinal/script.js
@@ -5,3 +5,14 @@ function openContactForm() {
   function closeContactForm() {
     document.getElementById("contactPopup").style.display = "none";
   }
+
+  function submitForm() {
+    var statusElement = document.querySelector(".submitStatus");
+      statusElement.innerText = "Submitted";
+
+      setTimeout(function() {
+        statusElement.innerText = "";
+      }, 5000);
+
+      document.getElementById("contactForm").reset();
+  }
