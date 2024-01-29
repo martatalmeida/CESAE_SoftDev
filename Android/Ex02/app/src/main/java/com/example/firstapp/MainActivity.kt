f158c5d726b779cbb7e6b86902fcd39a4a27d9f8
martatalmeida <martateixeiradealmeida@gmail.com>
2024-01-29 14:43:05 +0000

    android

--- Android/Ex02/app/src/main/java/com/example/firstapp/MainActivity.kt
+++ Android/Ex02/app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.firstapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firstapp.databinding.ActivityMainBinding
 
@@ -10,23 +11,42 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
-    /* Exercício de Conversão para dolares
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.buttonConverter.setOnClickListener{
-            var euros: Double = binding.editEuros.text.toString().toDouble()
+        binding.buttonReal.setOnClickListener{
+            var euros: Double = binding.editValorEuros.text.toString().toDouble()
 
-            var dolares : Double = euros*1.08
+            var reais : Double = euros*4.4
+            reais = String.format("%.2f", reais).toDouble()
+            val toast = Toast.makeText(this,"$reais R$", Toast.LENGTH_SHORT)
+            toast.show();
+        }
+
+        binding.buttonDolar.setOnClickListener{
+            var euros: Double = binding.editValorEuros.text.toString().toDouble()
+
+            var dolares : Double = euros*1.2
             dolares = String.format("%.2f", dolares).toDouble()
-            binding.textResultado.text = "$dolares $"
+            val toast = Toast.makeText(this,"$dolares $",Toast.LENGTH_SHORT)
+            toast.show();
+        }
+
+        binding.buttonPeso.setOnClickListener{
+            var euros: Double = binding.editValorEuros.text.toString().toDouble()
+
+            var pesos : Double = euros*31.5
+            pesos = String.format("%.2f", pesos).toDouble()
+            val toast = Toast.makeText(this,"$pesos M$",Toast.LENGTH_SHORT)
+            toast.show();
         }
     }
-    */
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+   /* override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -41,7 +61,7 @@ class MainActivity : AppCompatActivity() {
                 binding.textResultado.text = "Olá $nome $apelido"
             }
         }
-    }
+    }*/
 
 }
 
